0a70cd4e69005c071a064d1b6bb06770361ff792
prestonshotts <shotts.preston@gmail.com>
2021-07-07 07:25:19 -0700

    base working

---
+++ CoffeeShop/BeanVariety/BeanVariety.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class BeanVariety
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 3)]
+        public string Region { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
